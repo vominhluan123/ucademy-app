38c059198871d27d2911285ff752c193b5d7972a
luan <minhluan2555@gmail.com>
2025-02-06 15:32:29 +0700

    fix-webhook

---
+++ .history/src/app/(dashboard)/page_20250206153046.tsx
@@ -0,0 +1,19 @@
+import CourseItem from "@/components/course/CourseItem";
+import Heading from "@/components/typography/Heading";
+import createUser from "@/lib/actions/user.action";
+import React, { Fragment } from "react";
+
+const page = () => {
+  return (
+    <Fragment>
+      <Heading>Khám Phá</Heading>
+      <div className="grid grid-cols-3 gap-8 mt-8">
+        <CourseItem></CourseItem>
+        <CourseItem></CourseItem>
+        <CourseItem></CourseItem>
+      </div>
+    </Fragment>
+  );
+};
+
+export default page;
