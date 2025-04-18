3100f7f853a7817b5d457bbfabab78b91b096e20
luan <minhluan2555@gmail.com>
2025-04-18 16:41:45 +0700

    Update-Code

--- src/lib/actions/course.action.ts
+++ src/lib/actions/course.action.ts
@@ -8,7 +8,6 @@ import {
   TGetAllCourseParams,
   TUpdateCourseParams,
 } from "@/types";
-import { ECourseStatus } from "@/types/enum";
 import { FilterQuery } from "mongoose";
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../mongoose";
@@ -17,7 +16,7 @@ export async function getAllCourses(
   params: TGetAllCourseParams
 ): Promise<ICourse[] | undefined> {
   try {
-    connectToDatabase();
+    await connectToDatabase();
     const { page = 1, limit = 10, search, status } = params;
     const skip = (page - 1) * limit;
     const query: FilterQuery<typeof Course> = {};
@@ -40,18 +39,24 @@ export async function getAllCoursesPublic(
   params: TGetAllCourseParams
 ): Promise<ICourse[] | undefined> {
   try {
-    connectToDatabase();
+    await connectToDatabase();
     const { page = 1, limit = 10, search } = params;
     const skip = (page - 1) * limit;
-    const query: FilterQuery<typeof Course> = {};
+    const query: FilterQuery<typeof Course> = {
+      status: "APPROVED",
+    };
     if (search) {
       query.$or = [{ title: { $regex: search, $options: "i" } }];
     }
-    query.status = ECourseStatus.APPROVED;
     const courses = await Course.find(query)
       .skip(skip)
       .limit(limit)
       .sort({ created_at: -1 });
+    console.log(
+      "Found courses (public):",
+      courses.map((c) => ({ title: c.title, status: c.status }))
+    );
+
     return courses;
   } catch (error) {
     console.error(error);
